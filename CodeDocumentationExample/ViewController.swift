44be84ac6f572980984539cc8a6c2af5e46187c4
Peter Witham <peter.witham@gmail.com>
2017-08-03 01:29:32 -0500

    Created basic application to demonstrate documentation.

--- CodeDocumentationExample/ViewController.swift
+++ CodeDocumentationExample/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblDisplayText: UILabel!
+    @IBOutlet weak var txtInput: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /// Touching the button will call the _concatText_
+    /// function and use the return to set the display
+    /// label.
+    ///
+    /// - Parameter sender: _
+    @IBAction func btnDisplayText(_ sender: UIButton) {
+        lblDisplayText.text = concatText(inputString: txtInput.text!)
+    }
+
+
+    /// Takes a string argument and returns a concatenated
+    /// String.
+    ///
+    /// - Parameter inputString: Any string as input
+    /// - Returns: Returns a new string
+    func concatText(inputString:String) -> String {
+        let newText = "The Text is: " + inputString
+        return newText
+    }
 
 }
 
